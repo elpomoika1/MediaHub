42bbcf84d0d3b562bf32fd7bc3c4f9e8855027b3
elpomoika1 <zajcevv445@gmail.com>
2026-04-02 15:23:24 +0300

    feat: added userId in Value entity

--- src/main/java/me/elpomoika/MovieHub/domain/entity/Rating.java
+++ src/main/java/me/elpomoika/MovieHub/domain/entity/Rating.java
@@ -1,29 +1,13 @@
 package me.elpomoika.MovieHub.domain.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @NoArgsConstructor
-@Builder
+@SuperBuilder
 @AllArgsConstructor
-@Getter
-public class Rating {
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    @Min(1) @Max(10)
-    private double value;
-
-    @ManyToOne
-    private Media media;
+public class Rating extends BaseValue {
 }
--- src/main/java/me/elpomoika/MovieHub/domain/entity/Votes.java
+++ src/main/java/me/elpomoika/MovieHub/domain/entity/Votes.java
@@ -1,29 +1,14 @@
 package me.elpomoika.MovieHub.domain.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @NoArgsConstructor
-@Builder
+@SuperBuilder
 @AllArgsConstructor
-@Getter
-public class Votes {
-    @Id
-    @GeneratedValue
-    private Long id;
+public class Votes extends BaseValue {
 
-    @Min(1) @Max(10)
-    private double value;
-
-    @ManyToOne
-    private Media media;
 }
